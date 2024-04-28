dd30ea1a2163fc6a1ded89e88d7b14602ae22e09
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2024-04-28 15:17:44 -0400

    problem with repositories

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_template/infrastructure/presentation/screens/auth/welcome_screen.dart';
 import 'package:flutter_template/infrastructure/services/firebase/firebase_handler.dart';
 import 'package:flutter_template/infrastructure/services/firebase/firebase_options.dart';
 import 'package:flutter_template/infrastructure/presentation/config/theme/app_theme.dart';
 import 'package:flutter_template/application/blocs/notifications/notifications_bloc.dart';
-import 'package:flutter_template/infrastructure/presentation/screens/home_screen.dart';
 import 'package:flutter_template/infrastructure/services/notifications/notification_handler.dart';
 
 void main() async {
@@ -37,7 +37,7 @@ class MainApp extends StatelessWidget {
       theme: AppTheme().getTheme(),
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: const HomeScreen(),
+      home: const WelcomeScreen(),
     );
   }
 }
