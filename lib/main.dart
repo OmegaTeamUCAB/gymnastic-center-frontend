68191350ac7ec5986d458cd797e03035d959d5c0
Sspa1 <sandrospaa@gmail.com>
2024-04-30 03:32:08 -0400

    feat: :sparkles: Dynamic theme feature
    Provided an implementation of a dynamic theme feature (toggleDarkMode) using the BLoC pattern.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_template/application/blocs/theme/theme_bloc.dart';
 import 'package:flutter_template/infrastructure/presentation/screens/auth/welcome_screen.dart';
 import 'package:flutter_template/infrastructure/services/firebase/firebase_handler.dart';
 import 'package:flutter_template/infrastructure/services/firebase/firebase_options.dart';
@@ -21,6 +22,9 @@ void main() async {
       BlocProvider(
         create: (context) => NotificationsBloc(
             handler: NotificationHandler()..initializeLocalNotifications()),
+      ),
+      BlocProvider(
+        create: (context) => ThemeBloc()
       )
     ],
     child: const MainApp(),
@@ -33,8 +37,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      darkTheme: AppTheme().getDarkTheme(),
-      theme: AppTheme().getTheme(),
+      theme: context.select((ThemeBloc value) {
+        return value.state.themeData;
+      }),
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       home: const WelcomeScreen(),
