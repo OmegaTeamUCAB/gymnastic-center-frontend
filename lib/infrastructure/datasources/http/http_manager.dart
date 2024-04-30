79ed43756a2072defed46d9a7a5b3338a8873787
Sspa1 <sandrospaa@gmail.com>
2024-04-30 02:21:57 -0400

    feat: :sparkles: Http Connection Manager abstraction
    Provided an abstraction layer to abstract away the details of specific HTTP client implementations.

---
+++ lib/infrastructure/datasources/http/http_manager.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_template/application/core/result.dart';
+
+abstract class IHttpManager {
+  final String baseUrl;
+
+  IHttpManager({
+    required this.baseUrl
+  });
+
+  Future<Result<T>> makeRequest<T>({
+    required String httpMethod,
+    required String urlPath,
+    required T Function(dynamic) mapperCallBack,
+    Map<String, dynamic>? queryParams,
+    dynamic body,
+  });
+
+  void updateHeaders({
+    required String headerKey, 
+    required dynamic headerValue
+  });
+}
