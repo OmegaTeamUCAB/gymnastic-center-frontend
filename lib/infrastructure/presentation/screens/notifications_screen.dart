8d337d8d545ae884b08020d6c5cecfe5c78da6ae
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2024-05-02 23:07:37 -0400

    feat(infrastructure): :construction: notifications bloc implemented
    Implemented the notifications bloc on the notifications screen for showing amount of notifications and other stuff

--- lib/infrastructure/presentation/screens/notifications_screen.dart
+++ lib/infrastructure/presentation/screens/notifications_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_template/infrastructure/presentation/screens/home_screen.dart';
-import 'package:flutter_template/presentation/widgets/ui/brand_button.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_template/application/blocs/notifications/notifications_bloc.dart';
+import 'package:flutter_template/presentation/widgets/notifications/empty_notifications.dart';
+import 'package:flutter_template/presentation/widgets/notifications/notifications_list.dart';
 import 'package:flutter_template/presentation/widgets/ui/custom_app_bar.dart';
 
 class NotificationsScreen extends StatelessWidget {
@@ -8,88 +10,53 @@ class NotificationsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final notifications =
+        context.watch<NotificationsBloc>().state.notifications;
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size(double.infinity, 100),
-        child: CustomAppBar(
-            content: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Row(
-            children: [
-              const Text(
-                'Notifications',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
-              ),
-              const Spacer(),
-              const Text(
-                '0',
-                style: TextStyle(
+        appBar: PreferredSize(
+          preferredSize: const Size(double.infinity, 100),
+          child: CustomAppBar(
+              content: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              children: [
+                const Text(
+                  'Notifications',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                const Spacer(),
+                Text(
+                  notifications.length.toString(),
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                const Text(
+                  'Inbox',
+                  style: TextStyle(
                     color: Colors.white,
                     fontSize: 18,
-                    fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                width: 5,
-              ),
-              const Text(
-                'Inbox',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
+                  ),
                 ),
-              ),
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.delete_rounded,
-                    color: Colors.white,
-                  ))
-            ],
-          ),
-        )),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Image.asset(
-            'assets/bell_light.png',
-            width: 156,
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          Text(
-            'Cant find notifications',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 15,
-                color: Theme.of(context).colorScheme.onPrimary),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Text(
-            'Lets explore more content for you.',
-            style: TextStyle(
-                fontSize: 15, color: Theme.of(context).colorScheme.onSecondary),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          BrandButton(
-              width: double.infinity,
-              buttonText: 'Back to Feed',
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HomeScreen()),
-                );
-              })
-        ]),
-      ),
-    );
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.delete_rounded,
+                      color: Colors.white,
+                    ))
+              ],
+            ),
+          )),
+        ),
+        body: notifications.isEmpty
+            ? const EmptyNotifications()
+            : const NotificationsList());
   }
 }
