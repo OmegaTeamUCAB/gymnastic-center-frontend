7ad5757dd46ba0cac06da872c294d7e1fa44b7c1
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2024-05-08 10:29:06 -0400

    feat(Presentation): :lipstick: chips ui
    Basic ui for chips, still needs some improvements

--- lib/infrastructure/screens/search_screen.dart
+++ lib/infrastructure/screens/search_screen.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:gymnastic_center/application/models/search_filter.dart';
 import 'package:gymnastic_center/presentation/widgets/common/custom_app_bar.dart';
 import 'package:gymnastic_center/presentation/widgets/search/custom_search_bar.dart';
+
+final filterChips = [
+  const SearchFilter(label: 'Prenatal', value: 'prenatal'),
+  const SearchFilter(label: 'For Women', value: 'for_women'),
+  const SearchFilter(label: 'Training', value: 'training'),
+  const SearchFilter(label: 'Courses', value: 'courses'),
+  const SearchFilter(label: 'Videos', value: 'videos'),
+  const SearchFilter(label: 'Morning', value: 'yoga'),
+  const SearchFilter(label: 'Recent Post', value: 'recent_post'),
+  const SearchFilter(label: 'Most Popular', value: 'most_popular'),
+];
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -10,35 +22,72 @@ class SearchScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          SizedBox(
-            height: 130,
-            child: CustomAppBar(
-              content: Row(
-                children: [
-                  IconButton(
-                      iconSize: 30,
-                      onPressed: () => Navigator.pop(context),
-                      color: Colors.white,
-                      icon: const Icon(Icons.chevron_left)),
-                  const Text(
-                    'Popular Search',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                  )
-                ],
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                //makes searchBar clickable
+                height: 155,
               ),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15.0),
-            child: CustomSearchBar(),
+              CustomAppBar(
+                content: Row(
+                  children: [
+                    IconButton(
+                        iconSize: 30,
+                        onPressed: () => Navigator.pop(context),
+                        color: Colors.white,
+                        icon: const Icon(Icons.chevron_left)),
+                    const Text(
+                      'Popular Search',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+              const Positioned(
+                  bottom: 0,
+                  left: 0.0,
+                  right: 0.0,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: CustomSearchBar(),
+                  )),
+            ],
           ),
           const SizedBox(
-            height: 10,
+            height: 15,
           ),
-          const Text('Here is some content')
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Wrap(
+                children: filterChips.map((filter) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                    child: FilterChip(
+                      backgroundColor:
+                          Theme.of(context).colorScheme.surfaceTint,
+                      selectedColor: Theme.of(context).colorScheme.primary,
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                              color: Theme.of(context).colorScheme.surfaceTint),
+                          borderRadius: BorderRadius.circular(60)),
+                      label: Text(
+                        filter.label,
+                        style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary),
+                      ),
+                      onSelected: (bool value) {},
+                    ),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
         ],
       ),
     );
