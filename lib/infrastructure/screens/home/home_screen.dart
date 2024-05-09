a8db741fb57793d8e1f89d9b1093bdc36118679f
Sspa1 <sandrospaa@gmail.com>
2024-05-08 21:22:26 -0400

    refactor: :lipstick: Updated home screen layout and functionality
    Updated home screen layout and functionality of the application.

--- lib/infrastructure/screens/home/home_screen.dart
+++ lib/infrastructure/screens/home/home_screen.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gymnastic_center/application/blocs/course/course_bloc.dart';
+import 'package:gymnastic_center/infrastructure/screens/course/course_category_screen.dart';
+import 'package:gymnastic_center/presentation/widgets/course/course_carrusel.dart';
+import 'package:gymnastic_center/infrastructure/services/categories/CategoryService.dart';
+import 'package:gymnastic_center/presentation/widgets/common/carousel_header.dart';
 import 'package:gymnastic_center/presentation/widgets/home/home_app_bar.dart';
+import 'package:gymnastic_center/presentation/widgets/home/category_carousel.dart';
 
 class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: PreferredSize(
+    final popularCourses = context.watch<CourseBloc>().state.popularCourses;
+
+    return Scaffold(
+      appBar: const PreferredSize(
           preferredSize: Size(double.infinity, 230), child: HomeAppBar()),
-      body: Placeholder(),
+      body: Column(
+        children: [
+          CategoryCarousel(categoryRepository: CategoryService()),
+          CarouselHeader(
+            title: 'Popular courses',
+            onButtonPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => CourseCategoryScreen(
+                          categoryRepository: CategoryService(),
+                        )),
+              );
+              ;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: CourseCarrusel(
+              courses: popularCourses,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
