3f455f9c7a7a011590c244b4afbe80e7bbcbfea5
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2024-05-10 11:27:52 -0400

    separated files

--- lib/infrastructure/screens/course/course_detail_screen.dart
+++ lib/infrastructure/screens/course/course_detail_screen.dart
@@ -4,20 +4,17 @@ import 'package:gymnastic_center/application/blocs/course/course_bloc.dart';
 import 'package:gymnastic_center/domain/course/course.dart';
 import 'package:gymnastic_center/presentation/widgets/course/course_carrusel.dart';
 import 'package:gymnastic_center/presentation/widgets/common/custom_app_bar.dart';
+import 'package:gymnastic_center/presentation/widgets/course/course_detail_info_card.dart';
 
 class CourseDetailScreen extends StatefulWidget {
   final String courseId;
-  CourseDetailScreen({
-    super.key,
-    required this.courseId
-  });
+  const CourseDetailScreen({super.key, required this.courseId});
 
   @override
   State<CourseDetailScreen> createState() => _CourseDetailScreenState();
 }
 
 class _CourseDetailScreenState extends State<CourseDetailScreen> {
-
   @override
   void initState() {
     super.initState();
@@ -38,55 +35,68 @@ class _CourseDetailScreenState extends State<CourseDetailScreen> {
     final course = context.watch<CourseBloc>().state.course;
     final popularCourses = context.watch<CourseBloc>().state.popularCourses;
 
-    if (courseIsLoading) return Scaffold(body: Center(child: CircularProgressIndicator(strokeWidth: 2,)));
-    if (course == null) return Center(child: Text('No existe el curso'),);
+    if (courseIsLoading) {
+      return const Scaffold(
+          body: Center(
+              child: CircularProgressIndicator(
+        strokeWidth: 2,
+      )));
+    }
+    if (course == null) {
+      return const Center(
+        child: Text('No existe el curso'),
+      );
+    }
 
-    List<Course> updatedCourses = popularCourses.where((courses) => courses.id != course.id).toList();
+    List<Course> updatedCourses =
+        popularCourses.where((courses) => courses.id != course.id).toList();
     return Scaffold(
       appBar: PreferredSize(
-          preferredSize: Size(double.infinity, 350), child: Stack(
+          preferredSize: const Size(double.infinity, 350),
+          child: Stack(
             children: [
               Container(
-                margin: EdgeInsets.only(top: 100),
+                margin: const EdgeInsets.only(top: 100),
                 child: GestureDetector(
-                  onDoubleTap: () {
-                    
-                  },
+                  onDoubleTap: () {},
                   child: ClipRRect(
-                    borderRadius: BorderRadius.only(bottomRight: Radius.circular(60)),
+                    borderRadius: const BorderRadius.only(
+                        bottomRight: Radius.circular(60)),
                     child: Image.network(
-                      course!.imageUrl,
+                      course.imageUrl,
                       height: 350,
                       loadingBuilder: (context, child, loadingProgress) {
                         if (loadingProgress != null) {
-                          return Center(child: CircularProgressIndicator(),);
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
                         } else {
                           return child;
                         }
                       },
                       width: double.infinity,
                       fit: BoxFit.cover,
-                      ),
+                    ),
                   ),
                 ),
               ),
               CustomAppBar(
                 content: Row(
-                children: [
-                  IconButton(
-                      iconSize: 30,
-                      onPressed: () => Navigator.pop(context),
-                      color: Colors.white,
-                      icon: const Icon(Icons.chevron_left)),
-                  Text(
-                    course.name,
-                    style: TextStyle(
+                  children: [
+                    IconButton(
+                        iconSize: 30,
+                        onPressed: () => Navigator.pop(context),
                         color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
+                        icon: const Icon(Icons.chevron_left)),
+                    Text(
+                      course.name,
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
               ),
             ],
           )),
@@ -99,80 +109,64 @@ class _CourseDetailScreenState extends State<CourseDetailScreen> {
               child: Column(
                 children: [
                   Container(
-                    margin: EdgeInsets.symmetric(vertical: 11),
-                    child: Text(
-                                  course.description,
-                                  maxLines: 3, 
-                                  overflow: TextOverflow.ellipsis, 
-                                      )),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 15),
+                      margin: const EdgeInsets.symmetric(vertical: 11),
+                      child: Text(
+                        course.description,
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                      )),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 15),
                     child: Divider(),
                   ),
                   Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    _CouseDetailInfoCard(title: 'Level', captionNumber: course.level.toString(), iconData: Icons.menu,),
-                    _CouseDetailInfoCard(title: 'Weeks', captionNumber: course.weeks.toString(), iconData: Icons.calendar_month_sharp,),
-                    _CouseDetailInfoCard(title: 'Mins', captionNumber: course.minutes.toString(), iconData: Icons.watch_later_outlined,),
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 15),
-                  child: Align(                    
-                    alignment: Alignment.centerLeft,
-                    child: Text('More Most Popular Courses', style: Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 20)), 
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CourseDetailInfoCard(
+                        title: 'Level',
+                        captionNumber: course.level.toString(),
+                        iconData: Icons.menu,
+                      ),
+                      CourseDetailInfoCard(
+                        title: 'Weeks',
+                        captionNumber: course.weeks.toString(),
+                        iconData: Icons.calendar_month_sharp,
+                      ),
+                      CourseDetailInfoCard(
+                        title: 'Mins',
+                        captionNumber: course.minutes.toString(),
+                        iconData: Icons.watch_later_outlined,
+                      ),
+                    ],
                   ),
-                ),
-                SizedBox(
-                  height: 150,
-                  child: CourseCarrusel(courses: popularCourses, width: 180, onTap: (courseId) {
-                    context.read<CourseBloc>().add(GetCourseById(courseId: courseId));
-                  },))
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text('More Most Popular Courses',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge!
+                              .copyWith(fontSize: 20)),
+                    ),
+                  ),
+                  SizedBox(
+                      height: 150,
+                      child: CourseCarrusel(
+                        courses: popularCourses,
+                        width: 180,
+                        onTap: (courseId) {
+                          context
+                              .read<CourseBloc>()
+                              .add(GetCourseById(courseId: courseId));
+                        },
+                      ))
                 ],
               ),
             ),
-            
           ],
         ),
       ),
     );
-  }
-}
-
-class _CouseDetailInfoCard extends StatelessWidget {
-  final IconData iconData;
-  final String title;
-  final String captionNumber; 
-  _CouseDetailInfoCard({
-    
-    super.key, required this.iconData, required this.title, required this.captionNumber,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        height: 50,
-        child: Row(children: [
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 6),
-            height: double.infinity,
-            width: 50,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Theme.of(context).colorScheme.surfaceTint,
-            ),
-            child: Icon(iconData, color: Colors.deepPurple,),
-            
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-            Text(title),
-            Text(captionNumber)
-          ],)
-        ],),
-      );
   }
 }
