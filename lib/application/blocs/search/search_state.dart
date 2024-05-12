7cf43892dd0ad2d97760d1430f034a43b9d2119b
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2024-05-12 12:24:02 -0400

    feat(Presentation): :sparkles: loading in search
    Added the isLoading to search state and implemented it

--- lib/application/blocs/search/search_state.dart
+++ lib/application/blocs/search/search_state.dart
@@ -2,23 +2,27 @@ part of 'search_bloc.dart';
 
 class SearchState extends Equatable {
   final String searchTerm;
+  final bool isLoading;
   final Set<String> filters;
   final SearchResults results;
   const SearchState(
       {this.searchTerm = '',
+      this.isLoading = false,
       this.filters = const {},
       this.results = const SearchResults()});
 
   SearchState copyWith({
     String? searchTerm,
+    bool? isLoading,
     Set<String>? filters,
     SearchResults? results,
   }) =>
       SearchState(
         searchTerm: searchTerm ?? this.searchTerm,
+        isLoading: isLoading ?? this.isLoading,
         filters: filters ?? this.filters,
         results: results ?? this.results,
       );
   @override
-  List<Object> get props => [searchTerm, filters, results];
+  List<Object> get props => [searchTerm, isLoading, filters, results];
 }
