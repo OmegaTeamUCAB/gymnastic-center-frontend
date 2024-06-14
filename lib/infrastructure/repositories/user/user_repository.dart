263652558f7e24c81c2be8db42c7616c67705020
Andrex2605 <andresarispe2605@gmail.com>
2024-06-13 23:14:57 -0400

    urlPath and body fixed

--- lib/infrastructure/repositories/user/user_repository.dart
+++ lib/infrastructure/repositories/user/user_repository.dart
@@ -1,4 +1,6 @@
 
+import 'dart:convert';
+
 import 'package:gymnastic_center/application/use_cases/user/update_user.use_case.dart';
 import 'package:gymnastic_center/core/result.dart';
 import 'package:gymnastic_center/domain/auth/user_repository.dart';
@@ -14,17 +16,17 @@ class UserRepository extends IUserRepository{
   @override
   Future<Result<void>> updateUser(UpdateUserDto dto)async{
     final result = await _httpConnectionManager.makeRequest(
-      urlPath: '/user/update',
+      urlPath: 'user/update',
       httpMethod: 'PUT',
       mapperCallBack: (data) {
         return data;
       },
-      body: {
+      body: jsonEncode({
         'name': dto.fullName,
         'email': dto.email,
         'phoneNumber':dto.phoneNumber,
         'image':dto.image
-      }
+      })
     );
     return result;
   }
