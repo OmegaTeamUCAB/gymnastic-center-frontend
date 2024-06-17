f39032deb73594f606afcf7cb624f149851424f1
Andrex2605 <andresarispe2605@gmail.com>
2024-06-17 11:01:33 -0400

    trainer repository updated, and GetTrainersDto added

--- lib/domain/trainer/trainer_repository.dart
+++ lib/domain/trainer/trainer_repository.dart
@@ -1,7 +1,14 @@
 import 'package:gymnastic_center/core/result.dart';
 import 'package:gymnastic_center/domain/trainer/trainer.dart';
 
+class GetTrainersDto {
+  final int page;
+  final int? perPage;
+  final String? filter;
+  GetTrainersDto({this.perPage, required this.page, this.filter});
+}
+
 abstract class ITrainerRepository {
   Future<Result<Trainer>> getTrainerById(String id);
-  Future<Result<List<Trainer>>> getTrainers();
+  Future<Result<List<Trainer>>> getTrainers(GetTrainersDto dto);
 }
