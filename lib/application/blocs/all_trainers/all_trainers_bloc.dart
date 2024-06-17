3e6fffd7d054c597d46bc1722f78aec343ee6ae6
Andrex2605 <andresarispe2605@gmail.com>
2024-06-17 14:38:26 -0400

    AllTrainersBloc updated

--- lib/application/blocs/all_trainers/all_trainers_bloc.dart
+++ lib/application/blocs/all_trainers/all_trainers_bloc.dart
@@ -1,4 +1,4 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:gymnastic_center/application/use_cases/trainer/get_trainers.use_case.dart';
 import 'package:gymnastic_center/domain/trainer/trainer.dart';
@@ -12,16 +12,24 @@ class AllTrainersBloc extends Bloc<AllTrainersEvent, AllTrainersState> {
   AllTrainersBloc(this.getTrainersUseCase) : super(AllTrainersLoading()) {
     on<AllTrainersRequested>(_getAllTrainers);
   }
-  Future<void> _getAllTrainers(
-      AllTrainersRequested event, Emitter<AllTrainersState> emit) async {
+  Future<void> _getAllTrainers(AllTrainersRequested event, Emitter<AllTrainersState> emit) async {
+    print('cargando');
     emit(AllTrainersLoading());
-    final result = await getTrainersUseCase.execute(GetTrainersDto(page: event.page));
+    final result =
+        await getTrainersUseCase.execute(GetTrainersDto(page: event.page));
     if (result.isSuccessful) {
-      emit(AllTrainersSuccess(trainers: result.unwrap()));
+      final previousCourses = state is AllTrainersSuccess
+          ? (state as AllTrainersSuccess).trainers
+          : <Trainer>[];
+      final currentCourses = result.unwrap();
+      final allTrainers = [...previousCourses, ...currentCourses];
+      print('exitoso');
+      emit(AllTrainersSuccess(trainers: allTrainers));
     } else {
       try {
         throw result.unwrap();
       } catch (e) {
+        print('fallo');
         emit(AllTrainersFailed(message: e.toString()));
       }
     }
