761992572db044a969debe7df7e7dcd858acfbf8
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2024-06-18 23:11:14 -0400

    merge fix

--- lib/presentation/screens/trainer_screen.dart
+++ lib/presentation/screens/trainer_screen.dart
@@ -22,8 +22,7 @@ class _TrainerScreenState extends State<TrainerScreen> {
   void initState() {
     super.initState();
     _scrollController.addListener(_scrollListener);
-    trainerDetailBloc
-        .add(TrainerDetailRequested(trainerId: widget.trainer['id']));
+    trainerDetailBloc.add(TrainerDetailRequested(trainerId: widget.trainerId));
   }
 
   @override
