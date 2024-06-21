d2050da03619c3e02cf6cae0af444bace0a4c253
Andrex2605 <andresarispe2605@gmail.com>
2024-06-21 18:28:03 -0400

    feat(Presentation): Snackbars added and button behavior fixed

--- lib/presentation/widgets/trainer/trainer_list.dart
+++ lib/presentation/widgets/trainer/trainer_list.dart
@@ -21,80 +21,101 @@ class _TrainerListState extends State<TrainerList> {
 
   @override
   void initState() {
-    followTrainerBloc = GetIt.instance<FollowTrainerBloc>();
-    followStatus =
-        widget.trainers.map((trainer) => trainer.userFollow!).toList();
-    followers =
-        widget.trainers.map((trainer) => trainer.followers ?? 0).toList();
     super.initState();
+    followTrainerBloc = GetIt.instance<FollowTrainerBloc>();
+    followStatus = widget.trainers.map((trainer) => trainer.userFollow!).toList();
+    followers = widget.trainers.map((trainer) => trainer.followers ?? 0).toList();
   }
+
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: widget.trainers.asMap().entries.map((entry) {
-        int index = entry.key;
-        Trainer trainer = entry.value;
-        return Container(
-          margin: const EdgeInsets.only(bottom: 12),
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.tertiaryContainer,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: ListTile(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        TrainerScreen(trainerId: trainer.id!)),
-              );
-            },
-            title: Text(
-              trainer.name!,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
+    return BlocProvider.value(
+      value: followTrainerBloc,
+      child: Column(
+        children: widget.trainers.asMap().entries.map((entry) {
+          int index = entry.key;
+          Trainer trainer = entry.value;
+          return Container(
+            margin: const EdgeInsets.only(bottom: 12),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.tertiaryContainer,
+              borderRadius: BorderRadius.circular(10),
             ),
-            titleTextStyle: TextStyle(
+            child: ListTile(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => TrainerScreen(trainerId: trainer.id!),
+                  ),
+                );
+              },
+              title: Text(
+                trainer.name!,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              titleTextStyle: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.bold,
-                color: Theme.of(context).colorScheme.onPrimary),
-            subtitle:
-                Text('${followers[index]} followers | ${trainer.location}'),
-            leading: trainer.image != null
-                ? CircleAvatar(
-                    backgroundImage: NetworkImage(trainer.image!),
-                  )
-                : const Padding(
-                    padding: EdgeInsets.only(right: 8.0),
-                    child: Icon(
-                      Icons.person,
-                      size: 30,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+              subtitle: Text('${followers[index]} followers | ${trainer.location}'),
+              leading: trainer.image != null
+                  ? CircleAvatar(
+                      backgroundImage: NetworkImage(trainer.image!),
+                    )
+                  : const Padding(
+                      padding: EdgeInsets.only(right: 8.0),
+                      child: Icon(
+                        Icons.person,
+                        size: 30,
+                      ),
                     ),
-                  ),
-            trailing: BlocBuilder<FollowTrainerBloc, FollowTrainerState>(
-                builder: (context, state) {
+              trailing: BlocConsumer<FollowTrainerBloc, FollowTrainerState>(
+                listener: (context, state) {
                   if (state is FollowTrainerFailed) {
                     setState(() {
-                          followStatus[index] = !followStatus[index];
-                          if (followStatus[index]) {
-                            followers[index]++;
-                          } else {
-                            followers[index]--;
-                          }
-                        });
+                      followStatus[index] = !followStatus[index];
+                      followStatus[index]
+                          ? followers[index]++
+                          : followers[index]--;
+                    });
+
+                    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(
+                        followStatus[index]
+                            ? "Failed to unfollow. Please try again."
+                            : "Failed to follow. Please try again.",
+                      ),
+                      backgroundColor: Colors.red,
+                      duration: const Duration(seconds: 3),
+                    ));
+                  } else if (state is FollowTrainerSuccess) {
+                    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(
+                        followStatus[index]
+                            ? "Added trainer to your following list."
+                            : "Removed trainer from your following list.",
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      backgroundColor: const Color(0xFF4F14A0),
+                      duration: const Duration(seconds: 3),
+                    ));
                   }
+                },
+                builder: (context, state) {
                   return BrandButton(
                     onPressed: () {
                       setState(() {
                         followStatus[index] = !followStatus[index];
-                        if (followStatus[index]) {
-                          followers[index]++;
-                        } else {
-                          followers[index]--;
-                        }
+                        followStatus[index]
+                            ? followers[index]++
+                            : followers[index]--;
                       });
-
                       followTrainerBloc.add(FollowRequested(id: trainer.id!));
                     },
                     isVariant: followStatus[index],
@@ -103,10 +124,11 @@ class _TrainerListState extends State<TrainerList> {
                     text: followStatus[index] ? 'following' : 'follow',
                   );
                 },
-          ),
-        ));
-      }).toList(),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
     );
   }
-
 }
