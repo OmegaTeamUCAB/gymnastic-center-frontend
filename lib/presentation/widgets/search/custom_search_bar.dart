f1afc8c32b63b733d5bbd77b5db3b1c62760cbe9
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2024-06-24 09:36:56 -0400

    feat(Presentation): :lipstick: search screen redesign

--- lib/presentation/widgets/search/custom_search_bar.dart
+++ lib/presentation/widgets/search/custom_search_bar.dart
@@ -2,58 +2,72 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gymnastic_center/application/blocs/search/search_bloc.dart';
 import 'package:gymnastic_center/presentation/widgets/icons/gymnastic_center_icons.dart';
-import 'package:gymnastic_center/presentation/widgets/search/search_results_list.dart';
 
-class CustomSearchBar extends StatelessWidget {
+class CustomSearchBar extends StatefulWidget {
   const CustomSearchBar({super.key});
+
+  @override
+  State<CustomSearchBar> createState() => _CustomSearchBarState();
+}
+
+class _CustomSearchBarState extends State<CustomSearchBar> {
+  late final TextEditingController searchController;
+
+  @override
+  void initState() {
+    super.initState();
+    searchController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final searchBloc = context.watch<SearchBloc>();
-    final courses = searchBloc.state.results.courses;
-    final blogs = searchBloc.state.results.blogs;
-    return Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            color: const Color(0xFFF2F2F2),
-            borderRadius: BorderRadius.circular(6),
-            boxShadow: [
-              BoxShadow(
-                color: const Color(0xff2D2D3A).withOpacity(0.2),
-                offset: const Offset(0, 2),
-                blurRadius: 4.0,
-              ),
-            ],
-          ),
-          child: TextFormField(
-            style: const TextStyle(fontSize: 16, color: Colors.black),
-            cursorColor: const Color(0xFF4F14A0),
-            onChanged: (value) {
-              searchBloc.add(QueryStringChanged(value));
-            },
-            decoration: const InputDecoration(
-              hintStyle: TextStyle(fontSize: 16, color: Colors.grey),
-              suffixIconColor: Colors.grey,
-              hintText: 'Search',
-              suffixIcon: Icon(
-                GymnasticCenter.search,
-                size: 15,
-              ),
-              border: InputBorder.none,
-              contentPadding:
-                  EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            ),
-          ),
+
+    return TextFormField(
+      controller: searchController,
+      style: TextStyle(
+          fontSize: 16, color: Theme.of(context).colorScheme.onPrimary),
+      cursorColor: const Color(0xFF4F14A0),
+      onChanged: (value) {
+        searchBloc.add(QueryStringChanged(value));
+      },
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Theme.of(context).colorScheme.surface,
+        hintStyle: TextStyle(
+            color: Theme.of(context).colorScheme.onPrimary.withOpacity(0.8)),
+        suffixIconColor: Theme.of(context).colorScheme.outline,
+        hintText: 'What are you looking for?',
+        contentPadding: const EdgeInsets.symmetric(vertical: 6),
+        suffixIcon: searchController.text.isNotEmpty
+            ? IconButton(
+                icon: Icon(
+                  Icons.cancel_rounded,
+                  size: 20,
+                  color:
+                      Theme.of(context).colorScheme.onPrimary.withOpacity(0.5),
+                ),
+                onPressed: () {
+                  searchController.clear();
+                  searchBloc.add(const QueryStringChanged(''));
+                },
+              )
+            : null,
+        prefixIcon: const Icon(
+          GymnasticCenter.search,
+          size: 16,
         ),
-        const SizedBox(
-          height: 10,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide.none,
         ),
-        SearchResultsList(
-          courses: courses,
-          blogs: blogs,
-        ),
-      ],
+      ),
     );
   }
 }
