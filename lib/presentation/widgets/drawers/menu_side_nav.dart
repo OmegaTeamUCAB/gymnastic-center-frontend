56443f78c275307cbbbd6956990462bddff70775
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2024-06-24 16:09:15 -0400

    bottom nav bar ui fixes

--- lib/presentation/widgets/drawers/menu_side_nav.dart
+++ lib/presentation/widgets/drawers/menu_side_nav.dart
@@ -59,6 +59,7 @@ class MenuSideNav extends StatelessWidget {
                             width: 15,
                           ),
                           Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
                                 (authBloc.state as Authenticated).user.fullName,
