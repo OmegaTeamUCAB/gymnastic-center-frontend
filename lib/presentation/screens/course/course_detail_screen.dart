291a91730ccb42eeec23b7e694f36f25f7ef1d41
Sspa1 <sandrospaa@gmail.com>
2024-07-08 12:57:39 -0400

    style: :lipstick: Improved course detail screen
    Course detail screen UI improved to show course progress

--- lib/presentation/screens/course/course_detail_screen.dart
+++ lib/presentation/screens/course/course_detail_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:gymnastic_center/application/blocs/course_detail/course_detail_bloc.dart';
 import 'package:gymnastic_center/application/blocs/lesson/lesson_bloc.dart';
+import 'package:gymnastic_center/application/blocs/progress/progress_bloc.dart';
 import 'package:gymnastic_center/application/use_cases/course/get_course_by_id.use_case.dart';
 import 'package:gymnastic_center/presentation/screens/course/lesson_screen.dart';
 import 'package:gymnastic_center/presentation/widgets/common/brand_button.dart';
@@ -22,6 +23,9 @@ class CourseDetailScreen extends StatelessWidget {
                 getCourseByIdUseCase:
                     GetIt.instance.get<GetCourseByIdUseCase>())
               ..add(CourseRequested(id: courseId))),
+        BlocProvider.value(
+          value: GetIt.instance<ProgressBloc>(),
+        )
       ],
       child: _CourseView(
         courseId: courseId,
@@ -53,15 +57,18 @@ class _CourseViewState extends State<_CourseView> {
   void dispose() {
     _scrollController.removeListener(_scrollListener);
     _scrollController.dispose();
+    GetIt.instance<ProgressBloc>().setToInitialState();
     super.dispose();
   }
 
   void _scrollListener() {
-    if (_scrollController.offset >= 440) {
+    if (_scrollController.offset >= 540) {
+      if (_showFab == true) return;
       setState(() {
         _showFab = true;
       });
     } else {
+      if (_showFab == false) return;
       setState(() {
         _showFab = false;
       });
@@ -70,138 +77,138 @@ class _CourseViewState extends State<_CourseView> {
 
   @override
   Widget build(BuildContext context) {
-    final lessonBloc = GetIt.instance<LessonBloc>();
-
-    return BlocBuilder<CourseDetailBloc, CourseDetailState>(
-      builder: (context, state) {
-        if (state is CourseLoading) {
-          return const Scaffold(
-              body: Center(
-                  child: CircularProgressIndicator(
-            strokeWidth: 2,
-          )));
-        }
-        if (state is CourseError) {
-          return Center(
-            child: Text(state.message),
+    return BlocListener<CourseDetailBloc, CourseDetailState>(listener: (
+      context,
+      state,
+    ) {
+      if (state is CourseFetched) {
+          GetIt.instance<ProgressBloc>().add(LessonProgressRequested(
+            courseId: state.course.id,
+            )
           );
-        }
+          GetIt.instance<LessonBloc>().add(LoadCourseInfo(
+            course: state.course.id, 
+            courseImage: state.course.imageUrl, 
+            lessons: state.course.lessons!
+        ));
+      }
+    }, child: BlocBuilder<CourseDetailBloc, CourseDetailState>(
+        builder: (context, state) {
+      if (state is CourseLoading) {
+        return const Scaffold(
+            body: Center(
+                child: CircularProgressIndicator(
+          strokeWidth: 2,
+        )));
+      }
+      if (state is CourseError) {
+        return Center(
+          child: Text(state.message),
+        );
+      }
 
-        if (state is CourseFetched) {
-          final course = state.course;
-          lessonBloc.add(LoadCourseImage(course: course.imageUrl));
-          lessonBloc.add((LoadLessons(lessons: course.lessons!)));
+      if (state is CourseFetched) {
+        final course = state.course;
+        final progress = GetIt.instance<ProgressBloc>();
 
-          return BlocProvider.value(
-              value: lessonBloc,
-              child: BlocBuilder<LessonBloc, LessonState>(
-                builder: (context, state) {
-                  if (state is LessonLoading) {
-                    return const Scaffold(
-                        body: Center(
-                            child: CircularProgressIndicator(
-                      strokeWidth: 2,
-                    )));
-                  }
-
-                  if (state is LessonError) {
-                    return Center(
-                      child: Text(state.message),
-                    );
-                  }
-
-                  if (state is LessonLoaded) {
-                    return Scaffold(
-                      body: Stack(
-                        children: [
-                          Image.network(
-                            course.imageUrl,
-                            height: 400,
-                            loadingBuilder: (context, child, loadingProgress) {
-                              if (loadingProgress != null) {
-                                return const Center(
-                                  child: CircularProgressIndicator(),
-                                );
-                              } else {
-                                return child;
-                              }
-                            },
-                            width: double.infinity,
-                            fit: BoxFit.cover,
-                          ),
-                          CourseInfo(
-                            course: course,
-                            scrollController: _scrollController,
-                          ),
-                          AnimatedPositioned(
-                              duration: const Duration(milliseconds: 500),
-                              curve: Curves.easeOut,
-                              left: 0,
-                              right: 0,
-                              top: _showFab ? 0 : -140,
-                              child: Container(
-                                height: 110,
-                                width: double.infinity,
-                                color: const Color(0xFF4F14A0),
-                                child: Align(
-                                  alignment: Alignment.bottomCenter,
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(bottom: 10),
-                                    child: Text(
-                                      course.name,
-                                      style: const TextStyle(
-                                          fontSize: 24,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white),
-                                    ),
-                                  ),
-                                ),
-                              )),
-                          Positioned(
-                              top: 60,
-                              left: 10,
-                              child: Container(
-                                decoration: const BoxDecoration(
-                                  color: Color(0xFF4F14A0),
-                                  shape: BoxShape.circle,
-                                ),
-                                child: const BackButton(
-                                  color: Colors.white,
-                                ),
-                              )),
-                          AnimatedPositioned(
-                            duration: const Duration(milliseconds: 500),
-                            curve: Curves.easeOut,
-                            left: 10,
-                            right: 10,
-                            bottom: _showFab ? 30 : -100,
-                            child: BrandButton(
-                              text: AppLocalizations.of(context)!.startCourse,
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => LessonScreen(
-                                            lessonId: course.lessons!.first.id,
-                                          )),
-                                );
-                              },
+        return BlocProvider.value(
+          value: GetIt.instance<LessonBloc>(),
+          child: BlocBuilder<ProgressBloc, ProgressState>(
+            builder: (context, state) {
+              return Scaffold(
+                body: Stack(
+                  children: [
+                    Image.network(
+                      course.imageUrl,
+                      height: 400,
+                      loadingBuilder: (context, child, loadingProgress) {
+                        if (loadingProgress != null) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        } else {
+                          return child;
+                        }
+                      },
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                    Text(
+                      '${GetIt.instance<LessonBloc>().state.progress.percent}',
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                    CourseInfo(
+                      course: course,
+                      scrollController: _scrollController,
+                    ),
+                    AnimatedPositioned(
+                        duration: const Duration(milliseconds: 900),
+                        curve: (_showFab) ? Curves.easeOut : Curves.easeIn,
+                        left: 0,
+                        right: 0,
+                        top: _showFab ? 0 : -140,
+                        child: Container(
+                          height: 110,
+                          width: double.infinity,
+                          color: const Color(0xFF4F14A0),
+                          child: Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Padding(
+                              padding: const EdgeInsets.only(bottom: 10),
+                              child: Text(
+                                course.name,
+                                style: const TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
                             ),
-                          )
-                        ],
+                          ),
+                        )),
+                    Positioned(
+                        top: 60,
+                        left: 10,
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            color: Color(0xFF4F14A0),
+                            shape: BoxShape.circle,
+                          ),
+                          child: const BackButton(
+                            color: Colors.white,
+                          ),
+                        )),
+                    (progress.state.progressStatus != ProgressStatus.unitialized) ? Container() : AnimatedPositioned(
+                      duration: const Duration(milliseconds: 900),
+                      curve: (_showFab) ? Curves.easeOut : Curves.easeIn,
+                      left: 10,
+                      right: 10,
+                      bottom: _showFab ? 30 : -100,
+                      child: BrandButton(
+                        text: AppLocalizations.of(context)!.startCourse,
+                        onPressed: () async {
+                          await progress.startCourse(StartCourse(courseId: course.id), course.lessons!.first.id);
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LessonScreen(
+                                      lessonId: course.lessons!.first.id,
+                                    )),
+                          );
+                        },
                       ),
-                    );
-                  } else {
-                    return const Center(
-                      child: Text('Error'),
-                    );
-                  }
-                },
-              ));
-        } else {
-          return const Center(child: Text('Error'));
-        }
-      },
-    );
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
+        );
+      } else {
+        return const Center(
+            child: CircularProgressIndicator(
+          strokeWidth: 2,
+        ));
+      }
+    }));
   }
 }
--- lib/presentation/widgets/course/lesson_list.dart
+++ lib/presentation/widgets/course/lesson_list.dart
@@ -1,49 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:gymnastic_center/application/blocs/lesson/lesson_bloc.dart';
+import 'package:gymnastic_center/application/blocs/progress/progress_bloc.dart';
 import 'package:gymnastic_center/domain/lesson/lesson.dart';
-import 'package:gymnastic_center/presentation/screens/course/lesson_screen.dart';
+import 'package:gymnastic_center/presentation/screens/course/lesson_tile_info.dart';
 
 class LessonList extends StatelessWidget {
   final List<Lesson> lessons;
-  const LessonList({super.key, required this.lessons});
+  final void Function(int) onTap;
+  const LessonList({super.key, required this.lessons, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        padding: EdgeInsets.zero,
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        itemCount: lessons.length,
-        itemBuilder: (context, index) {
-          final lesson = lessons[index];
-          return Container(
-            margin: const EdgeInsets.only(bottom: 12),
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.surfaceTint,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: ListTile(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => LessonScreen(lessonId: lesson.id)),
-                );
-              },
-              title: Text(
-                lesson.title,
-                overflow: TextOverflow.ellipsis,
-              ),
-              titleTextStyle: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).colorScheme.onPrimary),
-              subtitle: Text(
-                lesson.content!,
-                overflow: TextOverflow.ellipsis,
-              ),
-              trailing: const Icon(Icons.info_outline),
-            ),
-          );
-        });
+    final lessonBloc = context.watch<LessonBloc>();
+    final lessonProgress =
+        context.watch<ProgressBloc>().state.progress.lessonProgress;
+
+    return BlocBuilder<ProgressBloc, ProgressState>(
+      builder: (context, state) {
+        return ListView.builder(
+            padding: EdgeInsets.zero,
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: lessons.length,
+            itemBuilder: (context, index) {
+              final lesson = lessons[index];
+
+              return Container(
+                margin: const EdgeInsets.only(bottom: 12),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surfaceTint,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: InkWell(
+                  onTap: () {
+                    onTap(index);
+                  },
+                  child: LessonTile(
+                    lesson: lesson,
+                    lessonProgress: (lessonProgress.isEmpty)
+                        ? null
+                        : lessonProgress.elementAt(index),
+                    selected: lessonBloc.state.lesson.id == lesson.id,
+                  ),
+                ),
+              );
+            });
+      },
+    );
   }
 }
