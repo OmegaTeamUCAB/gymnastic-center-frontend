9ba96eee6651ddf39b8852c02f1572fc6e6637e6
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2024-07-10 08:36:00 -0400

    fix(application): :ambulance: removed caching, sadly, it wasn't viable

--- lib/application/blocs/feature_courses/feature_courses_bloc.dart
+++ lib/application/blocs/feature_courses/feature_courses_bloc.dart
@@ -1,6 +1,5 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:gymnastic_center/application/blocs/resettable_bloc.dart';
 import 'package:gymnastic_center/application/use_cases/course/get_courses.use_case.dart';
 import 'package:gymnastic_center/domain/course/course.dart';
 import 'package:gymnastic_center/domain/course/course_repository.dart';
@@ -8,10 +7,9 @@ import 'package:gymnastic_center/domain/course/course_repository.dart';
 part 'feature_courses_event.dart';
 part 'feature_courses_state.dart';
 
-class FeatureCoursesBloc extends Bloc<FeatureCoursesEvent, FeatureCoursesState>
-    implements ResettableBloc {
+class FeatureCoursesBloc
+    extends Bloc<FeatureCoursesEvent, FeatureCoursesState> {
   final GetCoursesUseCase getCoursesUseCase;
-  final List<Course> _cachedCourses = [];
 
   FeatureCoursesBloc(this.getCoursesUseCase) : super(FeatureCoursesLoading()) {
     on<FeatureCoursesRequested>(_getFeatureCourses);
@@ -19,27 +17,17 @@ class FeatureCoursesBloc extends Bloc<FeatureCoursesEvent, FeatureCoursesState>
 
   Future<void> _getFeatureCourses(
       FeatureCoursesRequested event, Emitter<FeatureCoursesState> emit) async {
-    if (_cachedCourses.isNotEmpty) {
-      emit(FeatureCoursesSuccess(courses: _cachedCourses));
+    emit(FeatureCoursesLoading());
+    final result = await getCoursesUseCase
+        .execute(GetCoursesDto(page: event.page, filter: 'POPULAR'));
+    if (result.isSuccessful) {
+      emit(FeatureCoursesSuccess(courses: result.unwrap()));
     } else {
-      emit(FeatureCoursesLoading());
-      final result = await getCoursesUseCase
-          .execute(GetCoursesDto(page: event.page, filter: 'POPULAR'));
-      if (result.isSuccessful) {
-        _cachedCourses.addAll(result.unwrap());
-        emit(FeatureCoursesSuccess(courses: _cachedCourses));
-      } else {
-        try {
-          throw result.unwrap();
-        } catch (e) {
-          emit(FeatureCoursesFailed(message: e.toString()));
-        }
+      try {
+        throw result.unwrap();
+      } catch (e) {
+        emit(FeatureCoursesFailed(message: e.toString()));
       }
     }
-  }
-
-  @override
-  void reset() {
-    _cachedCourses.clear();
   }
 }
--- lib/application/blocs/resettable_bloc.dart
+++
@@ -1,3 +0,0 @@
-abstract class ResettableBloc {
-  void reset();
-}
